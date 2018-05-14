ec25fced53cbfbd13f6e84f1d23331a53e856421
LEPPEDIAZ <LEPPEDIAZ@users.noreply.github.com>
2018-05-14 15:04:07 -0300

    Update ejercicio3.sql

--- ejercicio3.sql
+++ ejercicio3.sql
@@ -7,3 +7,14 @@ from invoiceline as i
 	join album as al on al.albumid = t.albumid
 
 	join artist as ar on ar.artistid = al.artistid
+
+
+select t.name as 'track', t.composer, t.milliseconds, t.bytes, t.unitprice, a.title as 'album', g.name as 'genre', m.name as 'media type'
+
+from track as t
+
+	join album as a on a.albumid = t.albumid
+
+	join genre as g on g.genreid = t.genreid
+
+	join mediatype as m on m.mediatypeid = t.mediatypeid
